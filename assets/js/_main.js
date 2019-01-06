cd4f79bb9471c635aa047d824d37325685bc9344
Joseph Konan <jkonan@andrew.cmu.edu>
2019-01-05 20:44:52 -0500

    dataoptimal template

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -57,31 +57,12 @@ $(document).ready(function() {
     $(".initial-content").toggleClass("is--hidden");
     // set focus on input
     setTimeout(function() {
-      $(".search-content input").focus();
+      $("#search").focus();
     }, 400);
   });
 
-  // Smooth scrolling
-
-  // Bind popstate event listener to support back/forward buttons.
-  $(window).bind("popstate", function (event) {
-    $.smoothScroll({
-      scrollTarget: location.hash,
-      offset: -20
-    });
-  });
-  // Override clicking on links to smooth scroll
-  $('a[href*="#"]').bind("click", function (event) {
-    if (this.pathname === location.pathname && this.hash) {
-      event.preventDefault();
-      history.pushState(null, null, this.hash);
-      $(window).trigger("popstate");
-    }
-  });
-  // Smooth scroll on page load if there is a hash in the URL.
-  if (location.hash) {
-    $(window).trigger("popstate");
-  }
+  // init smooth scroll
+  $("a").smoothScroll({ offset: -20 });
 
   // add lightbox class to all image links
   $(
